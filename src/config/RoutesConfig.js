33ef59ac8311b3b90aa2f7fe9f4e8f962d8f00dc
mad-nectarine <mad.nectarine@gmail.com>
2016-02-05 10:55:54 +0900

    delete .js

--- src/config/RoutesConfig.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-function RouteConfig(app) {
-    app.use("/app", require("../routes/index"));
-    app.use("/data", require("../routes/data"));
-    app.use("/test", require("../routes/test"));
-}
-exports.default = RouteConfig;
